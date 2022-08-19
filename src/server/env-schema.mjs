b0b2e475fc64090ac9969dd97d2f65ed6f322dca
Walid Cherhane <wcherhane17@gmail.com>
2022-08-19 12:36:46 +0100

    remove unused env vars

--- src/server/env-schema.mjs
+++ src/server/env-schema.mjs
@@ -4,7 +4,6 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
-  NEXTAUTH_URL: z.string().url(),
   GITHUB_ID: z.string(),
   GITHUB_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
